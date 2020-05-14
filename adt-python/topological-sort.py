f131a0c3b33ff7bb8ed4ebf29aa231de4e690107
Hamid Bazargani <hamidbazargani@gmail.com>
2020-05-14 17:05:33 +0000

    Added python implementation of dag shortest path.

--- adt-python/topological-sort.py
+++ adt-python/topological-sort.py
@@ -3,15 +3,15 @@
 class Edge:
 
   def __init__(self, origin, to, w):
-    self._from = origin
-    self._to = to
-    self._w = w
+    self.from_ = origin
+    self.to = to
+    self.w = w
 
 def dfs_topsort(at, i, graph, visited, ordering):
   visited.add(at)
   for child in (graph.get(at) or []):
-    if child._to not in visited:
-      i = dfs_topsort(child._to, i, graph, visited, ordering)
+    if child.to not in visited:
+      i = dfs_topsort(child.to, i, graph, visited, ordering)
   ordering[i] = at
   return i-1
 
@@ -24,6 +24,28 @@ def topological_sort(graph):  # graph: Dict[int, List[Edge]]
     if at not in visited:
       i = dfs_topsort(at, i, graph, visited, ordering)
   return ordering
+
+# returns shortest path distance from start node to other nodes.
+# This only works for DAG.
+def dag_shortest_path(graph, start):
+  if len(graph) == 0:
+    return []
+  assert start in graph
+
+  top_sort = topological_sort(graph)
+  dist = len(graph) * [None]
+  dist[start] = 0  # distance from itself
+  for i in range(len(graph)):
+    node = top_sort[i]
+    if dist[node] is None: continue  # start form 'start' node.
+    for edge in (graph.get(node) or []):
+      new_dist = dist[node] + edge.w
+      if dist[edge.to] is None:
+        dist[edge.to] = new_dist
+      else:
+        dist[edge.to] = min(new_dist, dist[edge.to])
+  return dist
+
 
 if __name__ == '__main__':
   # Graph setup
@@ -39,3 +61,9 @@ if __name__ == '__main__':
   graph.get(5).append(Edge(5, 4, 7));
 
   assert topological_sort(graph) == [6, 0, 5, 1, 2, 3, 4]
+
+  dists = dag_shortest_path(graph, 0);
+  # Find the shortest path from 0 to 4 which is 8.0
+  assert dists[4] == 8.0
+  # Find the shortest path from 0 to 6 which is None since 6 is not reachable.
+  assert dists[6] is None
